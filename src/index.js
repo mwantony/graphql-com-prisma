3d57d66e1c12992095c15d46ec5e4109e754491e
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-29 09:21:21 -0300

    cap 3 vid 1 finalizado

--- src/index.js
+++ src/index.js
@@ -1,19 +1,41 @@
-const { ApolloServer, gql } = require('apollo-server')
+const { ApolloServer, gql } = require("apollo-server");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 const typeDefs = gql`
- type User {
-   nome: String
- }
- type Query {
-   users: [User]
- }
-`
+
+  scalar DateTime
+
+  type User {
+    id: Int
+    nome: String
+    email: String
+    createdAt: DateTime
+    posts: [Post]
+  }
+
+  type Post {
+    id: Int
+    titulo: String
+    conteudo: String
+  }
+
+  type Query {
+    users: [User]
+  }
+`;
 
 const resolvers = {
- Query: {
-   users: () => [{ nome: "Ana" }, {nome: "Bia" }]
- }
-}
+  Query: {
+    users: async () => await prisma.user.findMany({
+      include: {
+        posts: true
+      }
+    })
+  },
+};
 
-const server = new ApolloServer({ typeDefs, resolvers })
-server.listen({ port: 4000 }, () => console.log(`Servidor pronto em localhost:4000`)) 
+const server = new ApolloServer({ typeDefs, resolvers, context: prisma });
+server.listen({ port: 4000 }, () =>
+  console.log(`Servidor pronto em localhost:4000`)
+);

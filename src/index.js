cad99c605f2f0eb241c1274815bf3e44ffa2cf4f
Antony Maziero Warken <againplayi7@gmail.com>
2022-07-29 13:31:17 -0300

    cap 5 vid 1 finalizado

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ const { PrismaClient } = require("@prisma/client");
 
 const schema = require('./schema')
 
-const prisma = new PrismaClient({log: ["query"]});
+const prisma = new PrismaClient();
 
-const server = new ApolloServer({ schema, context:{prisma} });
+const server = new ApolloServer({ schema, context: {prisma} });
 server.listen({ port: 4000 }, () =>
   console.log(`Servidor pronto em localhost:4000`)
 );
